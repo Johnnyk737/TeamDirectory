196a71d410a1c949dbf198bf2aeb841735ade684
Johnnyk737 <johnnyk737@gmail.com>
2017-11-28 08:45:20 -0600

    Updating settings.py

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'www',
+    'Directory.www',
     'Directory',
     'gunicorn',
 ]
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Directory.wsgi.application'
+WSGI_APPLICATION = 'Directory.Directory.wsgi.application'
 
 
 # Database
